346f660239f3a56a466b072cbd7756ee9f1198cb
keaton.aranguaegbert <keaton.aranguaegbert@MU-C1-14.mullen.pvt.k12.co.us>
2025-03-25 13:14:48 -0600

    text started, nothing working yet

--- Pleasework-SIS/src/Runner.java
+++ Pleasework-SIS/src/Runner.java
@@ -6,24 +6,72 @@ public class Runner
 		public static void main (String args[])
 			{
 
-				Scanner scanner = new Scanner(System.in);
-				int number = scanner.nextInt();
 				
-				System.out.println("Would you like to:");
+				
+				System.out.println("Which alteration would you like to make?:");
 				System.out.println("1. Change Grade");
 				System.out.println("2. Switch Classes");
 				
+				Scanner scanner = new Scanner(System.in);
+				int number = scanner.nextInt();
+				
 				if(number == 1)
 					{
-						System.out.println("Which grade would you like to change?");
+						//need to access all information for student including grades
+						System.out.println("Which period grade would you like to change?");
+						System.out.println("1, 2, 3");
+						int periodGrade = scanner.nextInt();
+						
+						if(periodGrade == 1)
+							{
+							
+								System.out.println("What would you like to change their 1st period grade to?");
+								String gradeChange = scanner.nextLine();
+								System.out.println("Their grade is now " + gradeChange);
+							}
+						if(periodGrade == 2)
+							{
+								System.out.println("What would you like to change their 2nd period grade to?");
+								String gradeChange = scanner.nextLine();
+								System.out.println("Their grade is now " + gradeChange);
+							}
+						if(periodGrade == 3)
+							{
+								System.out.println("What would you like to change their 3rd period grade to?");
+								String gradeChange = scanner.nextLine();
+								System.out.println("Their grade is now " + gradeChange);
+							}
+						
 					}
 				if(number == 2)
 					{
-						System.out.println("Which class would you like to change?");
+						System.out.println("Which class period would you like to change?");
+						System.out.println("1, 2, 3");
+						
+						int periodChange = scanner.nextInt();
+						
+						if(periodChange == 1)
+							{
+								System.out.println("Their current first period class is ____. What first period would you like to change them into?");
+								String classInto = scanner.nextLine();
+								System.out.println("They have been changed into first period " + classInto);
+							}
+						if(periodChange == 2)
+							{
+								System.out.println("Their current second period class is ____. What second period would you like to change them into?");
+								String classInto = scanner.nextLine();
+								System.out.println("They have been changed into second period " + classInto);
+							}
+						if(periodChange == 3)
+							{
+								System.out.println("Their current third period class is ____. What third period would you like to change them into?");
+								String classInto = scanner.nextLine();
+								System.out.println("They have been changed into third period " + classInto);
+							}
 					}
-				else
+				else 
 					{
-						System.out.println("That is not an option.");
+						//System.out.println("That is not an option.");
 						
 					}
 
