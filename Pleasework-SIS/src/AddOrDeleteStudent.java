86ce4de48f7ed632a5476fde6bdbfef03299c7fb
keaton.aranguaegbert <keaton.aranguaegbert@MU-C1-14.mullen.pvt.k12.co.us>
2025-03-27 12:51:02 -0600

    push

--- Pleasework-SIS/src/AddOrDeleteStudent.java
+++ Pleasework-SIS/src/AddOrDeleteStudent.java
@@ -3,11 +3,13 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 
 
 public class AddOrDeleteStudent
 	{
-		static ArrayList<Student> studentList = new ArrayList<Student>();
 
 		public static void studentMain()
 			{
@@ -34,15 +36,7 @@ public class AddOrDeleteStudent
 		{
 			try
 				{
-			Scanner myFile = new Scanner(new File("StudentList.txt"));
-			String studFile = myFile.next();
-			
-			while(myFile.hasNext())
-				{
-				String[] prevStud = studFile.split(" ");
-				//studentList.add(new Student("Keaton", "Arangua", "Biology", "A", "English", "A", "Religion", "A"));
-				studentList.add(new Student(prevStud[0], prevStud[1], prevStud[2], prevStud[3], prevStud[4], prevStud[5], prevStud[6], prevStud[7]));
-				}
+					StudentArrayMaker.armain();
 				System.out.println("Please enter your student's fullname,"
 						+ " first period class, grade for first period,"
 						+ " second period class, grade for second period,"
@@ -50,27 +44,76 @@ public class AddOrDeleteStudent
 				Scanner studi = new Scanner(System.in);
 				String stud = studi.nextLine();
 				String[] studInfo = stud.split(" ");
-				studentList.add(new Student(studInfo[0], studInfo[1], studInfo[2], studInfo[3], studInfo[4], studInfo[5], studInfo[6], studInfo[7]));
-				System.out.println(studentList.size()-1);
+				StudentArrayMaker.studentList.add(new Student(studInfo[0], studInfo[1], studInfo[2], studInfo[3], studInfo[4], studInfo[5], studInfo[6], studInfo[7]));
+				for(Student S: StudentArrayMaker.studentList)
+					{
+						System.out.print(S.getFirstname());
+						System.out.print(" " + S.getLastname());
+						System.out.print(" " + S.getPeriodone());
+						System.out.print(" " + S.getPeriodoneg());
+						System.out.print(" " + S.getPeriodTwo());
+						System.out.print(" " + S.getPeriodTwog());
+						System.out.print(" " + S.getPeriodThree());
+						System.out.print(" " + S.getPeriodThreeg());
+						System.out.println("");
+					}
 				}
 			catch(NullPointerException x)
 				{
-				System.out.println("No more students.");
-				}
-			catch(FileNotFoundException y)
-				{
-				System.out.println("No file found.");
+				System.out.println("No more students. Try again");
 				}
 			catch(ArrayIndexOutOfBoundsException z)
 				{
-				System.out.println("Out of bounds.");
-				z.printStackTrace();
+				System.out.println("Out of bounds. Try again");
+				op1();
 				}
 		}
 		
 		public static void op2()
 		{
-			System.out.println("2");
+			StudentArrayMaker.armain();
+			try
+				{
+					System.out.println("What student would you like to delete?");
+					Scanner studr = new Scanner(System.in);
+					String stude = studr.nextLine();
+					String[] studInfod = stude.split(" ");
+					for(Student S: StudentArrayMaker.studentList)
+						{
+							studInfod[0]=S.getFirstname();
+							studInfod[1]=S.getLastname();
+							if(StudentArrayMaker.studentList.contains(studInfod[0]) && StudentArrayMaker.studentList.contains(studInfod[1]))
+								{
+									System.out.println("Found the student!");
+									for(Student T: StudentArrayMaker.studentList)
+										{
+											System.out.print(T.getFirstname());
+											System.out.print(" " + T.getLastname());
+											System.out.print(" " + T.getPeriodone());
+											System.out.print(" " + T.getPeriodoneg());
+											System.out.print(" " + T.getPeriodTwo());
+											System.out.print(" " + T.getPeriodTwog());
+											System.out.print(" " + T.getPeriodThree());
+											System.out.print(" " + T.getPeriodThreeg());
+											System.out.println("");
+										}
+								}
+							else {
+								System.out.println("Student doesn't exist. Please enter a different student");
+								op2();
+							}
+						}
+				}
+			catch(NullPointerException x)
+				{
+					System.out.println("No more students. Try again");
+					op2();
+				}
+			catch(ArrayIndexOutOfBoundsException z)
+				{
+				System.out.println("Out of bounds. Try again");
+				op2();
+				}
 		}
 
 	}
