47999bb5b2a072c4850fc0efae0b21259f456cc2
keaton.aranguaegbert <keaton.aranguaegbert@MU-C1-14.mullen.pvt.k12.co.us>
2025-03-31 10:29:50 -0600

    i think we done!

--- Pleasework-SIS/src/Runner.java
+++ Pleasework-SIS/src/Runner.java
@@ -3,28 +3,34 @@ import java.util.Scanner;
 
 public class Runner
 	{
+		static Scanner scanner = new Scanner(System.in);
+		//static int number = 0;
 		
 		public static void main (String args[])
 			{
 			StudentArrayMaker.armain();
+			System.out.println("Which student would you like to modify?");
+			printNames();
 			GradeOrClassChange();
+			//namesAndGrades();
+			//printNames();
 
 			}
 		
 		
 		public static void GradeOrClassChange()
 			{
-				System.out.println("Which student would you like to modify?");
-				printNames();
+				int number = scanner.nextInt();
 				
-					Scanner scanner = new Scanner(System.in);
-					int number = scanner.nextInt();
+				
+					
+					
 					
 					
 				System.out.println(StudentArrayMaker.studentList.get(number - 1).getFirstname() + " " + StudentArrayMaker.studentList.get(number - 1).getLastname()  );
-					System.out.println("1. " + StudentArrayMaker.studentList.get(number - 1).getPeriodone() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodoneg());
-					System.out.println("2. " + StudentArrayMaker.studentList.get(number - 1).getPeriodTwo() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodTwog());
-					System.out.println("3. " + StudentArrayMaker.studentList.get(number - 1).getPeriodThree() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodThreeg());
+				System.out.println("1. " + StudentArrayMaker.studentList.get(number - 1).getPeriodone() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodoneg());
+				System.out.println("2. " + StudentArrayMaker.studentList.get(number - 1).getPeriodTwo() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodTwog());
+				System.out.println("3. " + StudentArrayMaker.studentList.get(number - 1).getPeriodThree() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodThreeg());
 					
 					System.out.println();
 					System.out.println("What would you like to do?");
@@ -50,8 +56,18 @@ public class Runner
 									String newGrade = scanner.nextLine();
 									String gradeChange = scanner.nextLine();
 									StudentArrayMaker.studentList.get(number-1).setPeriodoneg(gradeChange);
-									System.out.println("P1 new grade is: " + StudentArrayMaker.studentList.get(number - 1).getPeriodone() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodoneg());
-									namesAndGrades();
+									//System.out.println("P1 new grade is: " + StudentArrayMaker.studentList.get(number - 1).getPeriodone() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodoneg());
+									System.out.println("New grades are:");
+									System.out.println();
+									
+									
+									
+									System.out.println(StudentArrayMaker.studentList.get(number - 1).getFirstname() + " " + StudentArrayMaker.studentList.get(number - 1).getLastname()  );
+									System.out.println("1. " + StudentArrayMaker.studentList.get(number - 1).getPeriodone() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodoneg());
+									System.out.println("2. " + StudentArrayMaker.studentList.get(number - 1).getPeriodTwo() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodTwog());
+									System.out.println("3. " + StudentArrayMaker.studentList.get(number - 1).getPeriodThree() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodThreeg());
+									
+									
 								}
 							if(periodGrade == 2)
 								{
@@ -62,8 +78,14 @@ public class Runner
 									String newGrade = scanner.nextLine();
 									String gradeChange = scanner.nextLine();
 									StudentArrayMaker.studentList.get(number-1).setPeriodTwog(gradeChange);
-									System.out.println("P2 new grade is: " + StudentArrayMaker.studentList.get(number - 1).getPeriodTwo() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodTwog());
-									namesAndGrades();
+									//System.out.println("P2 new grade is: " + StudentArrayMaker.studentList.get(number - 1).getPeriodTwo() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodTwog());
+									System.out.println("New grades are:");
+									System.out.println();
+									
+									System.out.println(StudentArrayMaker.studentList.get(number - 1).getFirstname() + " " + StudentArrayMaker.studentList.get(number - 1).getLastname()  );
+									System.out.println("1. " + StudentArrayMaker.studentList.get(number - 1).getPeriodone() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodoneg());
+									System.out.println("2. " + StudentArrayMaker.studentList.get(number - 1).getPeriodTwo() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodTwog());
+									System.out.println("3. " + StudentArrayMaker.studentList.get(number - 1).getPeriodThree() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodThreeg());
 								}
 							if(periodGrade == 3)
 								{
@@ -74,8 +96,14 @@ public class Runner
 									String newGrade = scanner.nextLine();
 									String gradeChange = scanner.nextLine();
 									StudentArrayMaker.studentList.get(number-1).setPeriodThreeg(gradeChange);
-									System.out.println("P3 new grade is: " + StudentArrayMaker.studentList.get(number - 1).getPeriodThree() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodThreeg());
-									namesAndGrades();
+									//System.out.println("P3 new grade is: " + StudentArrayMaker.studentList.get(number - 1).getPeriodThree() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodThreeg());
+									System.out.println("New grades are:");
+									System.out.println();
+									
+									System.out.println(StudentArrayMaker.studentList.get(number - 1).getFirstname() + " " + StudentArrayMaker.studentList.get(number - 1).getLastname()  );
+									System.out.println("1. " + StudentArrayMaker.studentList.get(number - 1).getPeriodone() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodoneg());
+									System.out.println("2. " + StudentArrayMaker.studentList.get(number - 1).getPeriodTwo() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodTwog());
+									System.out.println("3. " + StudentArrayMaker.studentList.get(number - 1).getPeriodThree() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodThreeg());
 								}
 							
 						}
@@ -91,25 +119,47 @@ public class Runner
 									System.out.println("Their current first period class is " + StudentArrayMaker.studentList.get(number-1).getPeriodone() + ". What first period would you like to change them into?");
 									String classInto = scanner.nextLine();
 									String newClass = scanner.nextLine();
-									System.out.println("They have been changed into first period " + newClass);
-									StudentArrayMaker.studentList.get(number-1).setPeriodone(classInto);
+									//System.out.println("They have been changed into first period " + newClass);
+									System.out.println("New schedule is:");
+									System.out.println();
+									StudentArrayMaker.studentList.get(number-1).setPeriodone(newClass);
+									
+									System.out.println(StudentArrayMaker.studentList.get(number - 1).getFirstname() + " " + StudentArrayMaker.studentList.get(number - 1).getLastname()  );
+									System.out.println("1. " + StudentArrayMaker.studentList.get(number - 1).getPeriodone() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodoneg());
+									System.out.println("2. " + StudentArrayMaker.studentList.get(number - 1).getPeriodTwo() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodTwog());
+									System.out.println("3. " + StudentArrayMaker.studentList.get(number - 1).getPeriodThree() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodThreeg());
+									
 								}
 							if(periodChange == 2)
 								{
 									System.out.println("Their current second period class is " + StudentArrayMaker.studentList.get(number-1).getPeriodTwo() + ". What second period would you like to change them into?");
 									String classInto = scanner.nextLine();
 									String newClass = scanner.nextLine();
-									System.out.println("They have been changed into second period " + newClass);
-									StudentArrayMaker.studentList.get(number-1).setPeriodTwo(classInto);
+									//System.out.println("They have been changed into second period " + newClass);
+									System.out.println("New schedule is:");
+									System.out.println();
+									StudentArrayMaker.studentList.get(number-1).setPeriodTwo(newClass);
 									System.out.println(StudentArrayMaker.studentList.get(number-1).getPeriodTwo());
+									
+									System.out.println(StudentArrayMaker.studentList.get(number - 1).getFirstname() + " " + StudentArrayMaker.studentList.get(number - 1).getLastname()  );
+									System.out.println("1. " + StudentArrayMaker.studentList.get(number - 1).getPeriodone() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodoneg());
+									System.out.println("2. " + StudentArrayMaker.studentList.get(number - 1).getPeriodTwo() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodTwog());
+									System.out.println("3. " + StudentArrayMaker.studentList.get(number - 1).getPeriodThree() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodThreeg());
 								}
 							if(periodChange == 3)
 								{
-									System.out.println("Their current third period class is " + StudentArrayMaker.studentList.get(number-1).getPeriodTwo() + ". What third period would you like to change them into?");
+									System.out.println("Their current third period class is " + StudentArrayMaker.studentList.get(number-1).getPeriodThree() + ". What third period would you like to change them into?");
 									String classInto = scanner.nextLine();
 									String newClass = scanner.nextLine();
-									System.out.println("They have been changed into third period " + newClass);
-									StudentArrayMaker.studentList.get(number-1).setPeriodThree(classInto);
+									//System.out.println("They have been changed into third period " + newClass);
+									System.out.println("New schedule is:");
+									System.out.println();
+									StudentArrayMaker.studentList.get(number-1).setPeriodThree(newClass);
+									
+									System.out.println(StudentArrayMaker.studentList.get(number - 1).getFirstname() + " " + StudentArrayMaker.studentList.get(number - 1).getLastname()  );
+									System.out.println("1. " + StudentArrayMaker.studentList.get(number - 1).getPeriodone() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodoneg());
+									System.out.println("2. " + StudentArrayMaker.studentList.get(number - 1).getPeriodTwo() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodTwog());
+									System.out.println("3. " + StudentArrayMaker.studentList.get(number - 1).getPeriodThree() + " " + StudentArrayMaker.studentList.get(number - 1).getPeriodThreeg());
 								}
 						}
 					else 
